e1289fcaae50160fd6d18c8e2935645b472ad521
Tesla606 <tressah2004@gmail.com>
2023-05-29 13:14:41 -0700

    Create function to handle character conversion specifier

---
+++ print_character.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdarg.h>
+#include <unistd.h>
+
+/**
+ * print_char - print characters 
+ * @list: number of parameters
+ * Return: success
+ */
+int print_char(va_list list)
+{
+	char c;
+
+	c = va_arg(list, int);
+	return (write(1, &c ,1);
+}
