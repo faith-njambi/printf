c95348085ea61e3f7813fe050411ff4023cc74a4
Faith Njambi <njambifaith30@gmail.com>
2023-05-31 01:08:48 +0300

    :recycle: size allocation using mallocs

--- print_character.c
+++ print_character.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdlib.h>
 
 /**
  * print_char - print characters
@@ -7,8 +8,18 @@
  */
 int print_char(va_list list)
 {
-	char c;
+	char *c;
+	int len;
 
-	c = va_arg(list, int);
-	return (write(1, &c, 1));
+	c = (char*)malloc(sizeof(char));
+	if (c)
+	{
+		*c = va_arg(list, int);
+		len = write(1, c, 1);
+		free(c);
+
+		return (len);
+	}
+	else
+		return (-1);
 }
